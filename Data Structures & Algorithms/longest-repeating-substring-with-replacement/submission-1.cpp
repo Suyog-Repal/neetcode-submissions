2a286b26d906c28cf238a9e7d1106e6f781b6a52
Suyog Repal <suyog.repal23@spit.ac.in>
2026-04-10 00:47:56 +0530

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char, int>freq; 
+        int maxi = INT_MIN; 
+        int maxFreq = 0; 
+       
+        
+        int l = 0; 
+        for(int r = 0; r<s.size(); r++){
+            freq[s[r]]++; 
+            if(maxFreq < freq[s[r]]){
+                maxFreq = freq[s[r]]; 
+            }
+            r-l+1 - maxFreq; 
+            while(r-l+1 - maxFreq > k){
+                freq[s[l]]--; 
+                if(freq[s[l]] == 0) freq.erase(s[l]); 
+                l++; 
+            }
+            maxi = max(maxi, r-l+1); 
+        }
+        return maxi; 
+    }
+};
