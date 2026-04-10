5e8e525068881d3aa177cac10473f57355c4b7a0
Suyog Repal <suyog.repal23@spit.ac.in>
2026-04-10 16:35:30 +0530

    Add: rotate-array - submission-4

---
+++ Data Structures & Algorithms/rotate-array/submission-4.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> swaps(vector<int>& nums, int l, int r){ 
+       
+        while(l<r){
+          swap(nums[l], nums[r]); 
+          l++; 
+          r--; 
+        }
+        return nums; 
+    }
+    void rotate(vector<int>& nums, int k) {
+    
+        int n = nums.size(); 
+        /// to avoid out of bound; 
+          k = k%n; 
+        swaps(nums, 0,  n-1);
+        swaps(nums, 0, k-1); 
+        swaps(nums, k , n-1); 
+    }
+};
