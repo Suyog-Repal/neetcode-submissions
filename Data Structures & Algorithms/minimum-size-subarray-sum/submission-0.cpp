91bc2146650ca3833b535305b7406fec37fd06af
Suyog Repal <suyog.repal23@spit.ac.in>
2026-04-12 01:26:36 +0530

    Add: minimum-size-subarray-sum - submission-0

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        unordered_map<int, int> freq; 
+        int l = 0; 
+      int sum = 0; 
+      int minLength = INT_MAX; 
+      for(int r = 0; r<nums.size(); r++){
+        sum += nums[r]; 
+        while(sum >= target){
+            minLength = min(minLength, r-l+1);
+            sum = sum - nums[l]; 
+            l++; 
+        }
+      }
+      if(minLength == INT_MAX) return 0; 
+      return minLength; 
+    }
+};
